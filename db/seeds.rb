309707a45b6b694d4035dc7f6ed03b3d13e681dd
samskinnerphd <samskinnerphd@gmail.com>
2017-07-21 20:58:55 -0400

    use Faker to create seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Team.destroy_all
+User.destroy_all
+
+# create 3 teams...
+3.times do
+  team = Team.create(
+    name: Faker::Team.name,
+    sport: Faker::Team.sport,
+  )
+  # ...with 5 users each
+  5.times do
+    team.users.create(
+      name: Faker::Name.unique.name,
+      # no need to seed team_id, it'll do it for you. MAGIC!
+    )
+  end
+end
